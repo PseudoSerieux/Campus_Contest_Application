f7f3b32723ce4930b7fde6ca003fc7f3edfb8b7a
Bufaloo <killian-r@live.fr>
2019-12-19 15:16:30 +0100

    suppression du doublon d'un dossier

--- Web app/pages/index.vue
+++ Web app/pages/index.vue
@@ -4,6 +4,9 @@
     justify-center
     align-center
   >
+      <div>
+          <v-btn medium color="#9EFD38">Télécharger l'application<br>dès maintenant !</v-btn>
+      </div>
   </v-layout>
 </template>
 
