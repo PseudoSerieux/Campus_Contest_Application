075a323f51e14483bd9c3db1562aa2f410e19f14
corentinft <corentin.fouquet@outlook.com>
2019-12-19 10:06:01 +0100

    changement de techno (reactjs -> vuejs)

---
+++ Web app/layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    />
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      fixed: false
+    }
+  }
+}
+</script>
