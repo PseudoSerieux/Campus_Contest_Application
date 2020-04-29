f37f02b864293bfd292eac540f34c7900f1f4331
Margaux <margaux.dechaud@imie.fr>
2020-04-29 15:45:32 +0200

    Suppression de l'ancien dossier pour le site

--- Web app/pages/who.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-layout>
-    <v-flex offset-sm1 sm1>
-      <v-img :src="require('../static/logo_CA.jpg')" />
-    </v-flex>
-    <v-flex offset-sm1 sm8>
-    </v-flex>
-  </v-layout>
-</template>
