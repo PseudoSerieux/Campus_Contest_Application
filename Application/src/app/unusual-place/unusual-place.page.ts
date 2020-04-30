c30428f082d7b0904cc257aef3d1439addea535a
NicolasHAMY <nhamypro@gmail.com>
2020-04-30 09:46:41 +0200

    Modification majeure | Importation d'une map V1 via Leaflet

--- Application/src/app/unusual-place/unusual-place.page.ts
+++ Application/src/app/unusual-place/unusual-place.page.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import {AngularFireDatabase} from '@angular/fire/database';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Map, latLng, tileLayer, marker, Layer} from 'leaflet';
+import {NavController} from '@ionic/angular';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-unusual-place',
@@ -7,10 +12,18 @@ import {AngularFireDatabase} from '@angular/fire/database';
   styleUrls: ['./unusual-place.page.scss'],
 })
 export class UnusualPlacePage implements OnInit {
-
   constructor(public afDB: AngularFireDatabase) { }
 
   ngOnInit() {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('Voici ma pop-up de test')
+        .openPopup();
   }
 /*
   add() {
@@ -23,4 +36,6 @@ export class UnusualPlacePage implements OnInit {
     this.afDB.list('Randonnees').remove();
   }
 */
+
+
 }
