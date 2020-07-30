16abbae6bebb7806491750e028d2528ab2922b85
NicolasHAMY <nhamypro@gmail.com>
2020-07-30 12:33:58 +0200

    Amélioration du code page randonnée et caméra | Affichage d'un trajet et amélioration du code

--- Application/src/app/maps/maps.page.ts
+++ Application/src/app/maps/maps.page.ts
@@ -27,10 +27,12 @@ export class MapsPage implements OnInit {
       attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+    setTimeout(function(){ map.invalidateSize()}, 400);
 
     L.marker([57.73, 11.94]).addTo(map)
         .bindPopup('Pop-up test')
         .openPopup();
+
     L.Routing.control({
           waypoints: [
             L.latLng(57.74, 11.94),
@@ -46,8 +48,29 @@ export class MapsPage implements OnInit {
   getPointDatabase() {
     for (let i = 0; i <= 573; i++) {
     this.afDB.list('randonnees/features/' + i + '/properties/').snapshotChanges(['child_added']).subscribe(points => {
+      //console.log(points);
+     // console.log(points[0]);
+
       points.forEach(point => {
-        console.log('Point: ' + point.payload.exportVal());
+        
+        //console.log(point);
+        if (point.key == "LONGITUDE") {
+          //console.log(point.key);
+          //console.log('dans la boucle');
+          const longitude = [point.payload.exportVal()];
+          return longitude;
+
+        } else if (point.key == "LATITUDE") {
+          //console.log(point.key);
+          //console.log('dans la boucle');
+          const latitude = [point.payload.exportVal()];
+
+          return latitude
+
+        } else {
+          return "";
+        }
+
 
       });
     });
